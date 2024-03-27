956edf0ef542d5bba22478fa2c6185e235cd2d2d
ZIPcde <votmoyyashik100@gmail.com>
2024-03-27 22:12:14 +0300

    savig before css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { Message } from "./components/message.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1> Hello world!</h1>
+      <Message message="First props"/>
+      <Message message="Second props"/>
+      <Message message="Third props"/>
+    </>
   );
 }
 
